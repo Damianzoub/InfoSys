07433915e610698f5eae05de9660a9cb7dbc9f5a
AlexiaGk159 <alexiagk159@gmail.com>
2026-05-13 21:06:52 +0300

    Auth Page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import PetListPage from './pages/PetListPage'
 import PetProfilePage from './pages/PetProfilePage'
+import AuthPage from './pages/AuthPage'
 import './App.css'
 
 function App() {
@@ -13,9 +14,7 @@ function App() {
           <Route path="/" element={<Navigate to="/pets" replace />} />
           <Route path="/pets" element={<PetListPage />} />
           <Route path="/pets/:id" element={<PetProfilePage />} />
-          {/* Βήμα 6 — Auth routes (Αλεσία) */}
-          {/* <Route path="/login"    element={<LoginPage />} /> */}
-          {/* <Route path="/register" element={<RegisterPage />} /> */}
+          <Route path="/auth" element={<AuthPage />} />
         </Routes>
       </main>
     </BrowserRouter>
